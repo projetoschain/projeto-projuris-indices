6dd269a4b6e7670a7b683edffd84ed92e21ef0ac
Bruno Hdseising <brunohdseising@gmail.com>
2023-05-26 04:27:51 -0300

    é isso paeeee

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
 import { FaHome, FaMoneyBill } from "react-icons/fa";
-import useUser from "../../hooks/useUser";
 import * as S from "./styles";
 
 const Header = () => {
-  const { data } = useUser();
 
   return (
     <S.Container>
@@ -14,20 +12,11 @@ const Header = () => {
         </S.NavIcon>
       </S.NavItems>
       <S.Main>
-        <S.LogoArea>
-          <Image
-            alt="logo"
-            src="/logo2.png"
-            width={100}
-            height={100}
-            style={{ width: "65%", height: "100%" }}
-          />
-        </S.LogoArea>
       </S.Main>
       <S.User>
-        <S.NavIcon href="/dashboard">
+        <S.NavIcon href="/fonte">
           <FaMoneyBill size={30} />
-          <b> Smart Tickets</b>
+          <b> Projuris</b>
         </S.NavIcon>
       </S.User>
     </S.Container>
--- src/components/templates/Calc/sections/Calc/index.tsx
+++ src/components/templates/Calc/sections/Calc/index.tsx
@@ -5,7 +5,7 @@ import * as IS from "./styles";
 
 const Calc = () => {
   return (
-    <S.Section id="events">
+    <S.Section id="calc">
       <IS.Container>
         <S.H1>A Calculadora de indices sera exibida aqui!, falta pouco....</S.H1>
         <IS.CardsArea>
